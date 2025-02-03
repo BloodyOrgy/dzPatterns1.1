9aa6fd615ba60f187f94dac59ec27920ba9b6c0b
Satana <rad_devil@inbox.ru>
2025-02-03 22:13:30 +0400

    updateDataGenerator

--- src/test/java/data/DataGenerator.java
+++ src/test/java/data/DataGenerator.java
@@ -53,11 +53,52 @@ public class DataGenerator {
             }
     }
 
-    @Value
-    public static class UserInfo {
-        String city;
-        String name;
-        String phone;
+ public static final class UserInfo {
+    private final String city;
+    private final String name;
+    private final String phone;
 
+    public String getCity(){
+    return this.city;
     }
+    public String getName(){
+        return this.name;
+    }
+    private String getPhone(){
+        return this.phone;
+    }
+
+     @Override
+     public boolean equals(Object o) {
+         if (o == this) return true;
+         if (!(o instanceof UserInfo)) return false;
+         final UserInfo other = (UserInfo) o;
+         final Object this$city = this.getCity();
+         final Object object$city = other.getCity();
+         if (this$city == null ? other$city != null : !this$city.equals(other$city)) return false;
+         final Object this$name = this.getName();
+         final Object other$name = other.getName();
+         if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
+         final Object this$phone = this.getPhone();
+         final Object other$phone = other.getPhone();
+         if (this$phone == null ? other$phone != null : !this$phone.equals(other$phone)) return false;
+         return true;
+     }
+         public int hashCode(){
+        final  int PRIME = 59;
+        int result =1;
+        final Object $city =this.getCity();
+        result = result * PRIME + ($city == null ? 43 : $city.hashCode());
+        final Object $name = this.getName();
+        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
+        final Object $phone = this.getPhone();
+        result = result * PRIME + ($phone == null ? 43 : $phone.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "DataGenerator.UserInfo(city=" + this.getCity() + ", name=" + this.getName() + ", phone=" + this.getPhone() + ")";
+    }
+ }
+
 }
